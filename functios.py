e38e1d4c4f55af7a9e7b379fcf6ee9f72de092f6
Ivy Cherotich <ivycherotich21@gmail.com>
2023-04-04 15:53:59 +0300

    pyTHON

--- functios.py
+++ functios.py
@@ -11,6 +11,7 @@ y=3
 print(x)
 print(y)
 print(x*y)
+print(x+y)
 
 name = input("what is your name?")
 print(name)
@@ -26,7 +27,7 @@ print(f"{number_one* number_two}")
 number_one = int(input("what is your first"))
 number_two =int( input("what is your second"))
 final_number=number_one +number_two
-print(f"{final_number}")#
+print(f"{final_number}")# prints formatted number
 print("{} + {} = {}".format(number_one,number_two,final_number))
 
 
