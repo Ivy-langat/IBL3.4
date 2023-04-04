4cd58543678d3fe541f2295bfc556423f03680c6
Ivy Cherotich <ivycherotich21@gmail.com>
2023-04-04 15:17:20 +0300

    This is python programming

---
+++ functios.py
@@ -0,0 +1,35 @@
+#This is the print function
+""" This is a print funtion"""
+print("Hello, World!")
+print(2)
+print(3.4)
+
+
+#variables
+x=2
+y=3
+print(x)
+print(y)
+print(x*y)
+
+name = input("what is your name?")
+print(name)
+
+first_number = int(input("what is your first"))
+second_number = int(input("what is your second"))
+print(first_number *second_number)
+
+number_one = int(input("what is your first"))
+number_two =int( input("what is your second"))
+print(f"{number_one* number_two}")
+
+number_one = int(input("what is your first"))
+number_two =int( input("what is your second"))
+final_number=number_one +number_two
+print(f"{final_number}")#
+print("{} + {} = {}".format(number_one,number_two,final_number))
+
+
+Z=float(input("what is your Z?"))
+R=float(input("what is your R?"))
+print(Z*R)
