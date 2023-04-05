06e646705ccdf46470704e86116440fd6becad5a
Ivy Cherotich <ivycherotich21@gmail.com>
2023-04-05 16:30:42 +0300

    this is fefinitions

---
+++ definitions.py
@@ -0,0 +1,16 @@
+x= int(input("Enter the value of x "))
+y= int(input("Enter the value of y "))
+
+#function for addition
+def addition() :
+ print(x + y)
+
+addition ()
+
+#working with strings
+name= input("what is your name?")
+
+def hello(z="WORLD") :
+ print("hello,",z)
+
+hello(name) #inputs the variable name
