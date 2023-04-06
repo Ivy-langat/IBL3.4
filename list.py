466a64c4b906c06c3c54d914dcf7d929d2efab52
Ivy Cherotich <ivycherotich21@gmail.com>
2023-04-06 15:47:03 +0300

    lists

---
+++ list.py
@@ -0,0 +1,20 @@
+#students = ["Kevin", "Sheldon", "Brian", "Johnte"]
+
+#print(students[0])
+#print(students[1])
+#print(students[2])
+
+
+#when using loop
+#students = ["Kevin", "Sheldon", "Brian", "Johnte"]
+
+#for student in students:
+  #  print(student)
+
+
+#using append to add
+students = ["Kevin", "Sheldon", "Brian", "Johnte"]
+
+students.append("Joy")
+for student in students:
+    print(student)
