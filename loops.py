47c3cdb1e4a6c7738367d065ee4b493ae65fac83
Ivy Cherotich <ivycherotich21@gmail.com>
2023-04-06 15:11:20 +0300

    while and for loops

---
+++ loops.py
@@ -0,0 +1,14 @@
+#number_of_times = 3
+#while number_of_times != 0:
+ #   print("hello, world")
+ 
+  #  number_of_times= number_of_times - 1
+
+"for loop"
+#number_of_times = 4
+#for number_of_times in [0, 1, 2, 3]:
+  #  print("hello, world")
+
+"for loop used by senior engineers"
+for _ in range (6):
+    print("hello, world")
