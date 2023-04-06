02b8fab3037eb88a112a0f8839f451a28c647b45
Ivy Cherotich <ivycherotich21@gmail.com>
2023-04-06 15:07:30 +0300

    gradeusingand

---
+++ gradeusingand.py
@@ -0,0 +1,13 @@
+score = int(input("Score"))
+
+if score >=90 and score <=100:
+    print("Grade: a")
+
+elif score >=80 and score <=90:
+    print("Grade: b")
+elif score >=70 and score <=80:
+    print("Grade: c")
+elif score >=60 and score <=70:
+    print("Grade: d") 
+else:
+        print("Grade: f") 
