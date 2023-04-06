54a9f14fea5185c4b30312fe1ce4009319074548
Ivy Cherotich <ivycherotich21@gmail.com>
2023-04-06 14:40:10 +0300

    implements the answer to the great question of life, the universe and everything

---
+++ deep.py
@@ -0,0 +1,15 @@
+Answer= input("what is the answer to the great question of life, the universe and everything? ")
+
+if Answer == "42":
+    print("yes")
+
+elif Answer.lower() == "forty-two":
+        print("yes")
+
+elif Answer.lower() == "forty two":
+        print("yes")     
+
+#otherwise input no
+else:
+    print("no")
+
